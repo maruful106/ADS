bafba9f6f3f1dd765f5fcca3b3647de058c946a2
A.K.M Maruful Islam <148320455+maruful106@users.noreply.github.com>
2023-11-08 20:55:59 +0600

    Initial Commit with Two Sum problem solving.
    - Solved the initial two sum problem
    - Added gitIgnore
    - Added Benchmark

---
+++ ADS/Problems/TwoSum.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+namespace ADS.Problems
+{
+    public class TwoSum
+    {
+        public int[] TwoSumMethod(int[] nums, int target)
+        {
+            Dictionary<int, int> numDict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (numDict.TryGetValue(complement, out int index))
+                {
+                    return new int[] { index, i };
+                }
+                numDict[nums[i]] = i;
+            }
+            throw new ArgumentException("No two sum solution");
+        }
+    }
+}
---
+++ ADS/Program.cs
@@ -0,0 +1,21 @@
+﻿using ADS.Problems;
+using BenchmarkDotNet.Running;
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        TwoSum twoSum = new TwoSum();
+        int[] data = new int[100000000];
+        for (int i = 0; i < 99999999; i++)
+        {
+            data[i] = i;
+        }
+        Console.WriteLine(data.Length);
+
+        int[] result = twoSum.TwoSumMethod(data, 180000000);
+        Console.WriteLine("Result:{0},{1}", result[0], result[1]);
+    }
+}
