bef7b9b0d98179fe3f0ecc09d1f039e08cdb6772
A.K.M Maruful Islam <148320455+maruful106@users.noreply.github.com>
2023-11-09 12:29:21 +0600

    MoveZeroes

---
+++ ADS/Problems/MoveZeroes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADS.Problems
+{
+    public class MoveZeroes
+    {
+        public int[] MoveZeroesMethod(int[] movements)
+        {
+            int pointer = 0;
+            for (int i = 0; i < movements.Length; i++)
+            {
+                if (movements[i] != 0)
+                {
+                    movements[pointer] = movements[i];
+                    pointer++;
+                }
+            }
+            for(int i = pointer; i< movements.Length; i++)
+            {
+                movements[i] = 0;
+            }
+            return movements;
+        }
+    }
+}
--- ADS/Program.cs
+++ ADS/Program.cs
@@ -9,13 +9,16 @@ internal class Program
         Random random = new Random();
 
 
-        MajorityElement obj = new MajorityElement();
-        int[] data = new int[] {2,1,1, 1, 2, 2, 2,2,2,1,1,3,2};
+        MoveZeroes obj = new MoveZeroes();
+        int[] data = new int[] {-9,1,0,39,2,1};
         //for (int i = 0; i < 9; i++)
         //{
         //    data[i] = random.Next(20);
         //}
-        int result = obj.MajorityElementMethod(data);
-        Console.WriteLine("Result:{0}", result);
+        int[] result = obj.MoveZeroesMethod(data);
+        foreach (int i in result)
+        {
+            Console.WriteLine("Result:{0}", i);
+        }
     }
 }
