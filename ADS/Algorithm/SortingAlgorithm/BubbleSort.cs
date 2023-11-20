4c1e2ba69df7d89b22fc56d21eb1053a4d57a3b0
A.K.M Maruful Islam <148320455+maruful106@users.noreply.github.com>
2023-11-20 17:57:26 +0600

    Bubble sort implementation

---
+++ ADS/Algorithm/SortingAlgorithm/BubbleSort.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Diagnostics.Tracing.Parsers.MicrosoftAntimalwareEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADS.Algorithm.SortingAlgorithm
+{
+    /// <summary>
+    /// Bubble sort is a simple sorting algorithm that works by repeatedly stepping through the list of elements, 
+    /// comparing each pair of adjacent items, and swapping them if they are in the wrong order
+    /// 
+    /// ##### procedural code #####
+    /// procedure bubbleSort(list: array of comparable)
+    /// n:= lenght(list)
+    /// repeat
+    ///     swapped:= false
+    ///     for i:= 1 to n-1
+    ///         if list[i-1]> list[i]
+    ///             swap(list[i-1], list[i])
+    ///             swapped:= true
+    ///     end for
+    ///     n:= n-1
+    /// until dnot swapped
+    /// 
+    /// </summary>
+    /// <param name="InputArray">Array of integer which need to be sorted using bubble sort.</param>
+    /// <returns>Sorted Array.</returns>
+    public class BubbleSort
+    {
+        public int[] SortAsc(int[] list) 
+        {
+            int n = list.Length;
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < n; i++)
+                {
+                    if (list[i] < list[i - 1])
+                    {
+                        int temp = list[i];
+                        list[i] = list[i - 1];
+                        list[i - 1] = temp;
+                        swapped = true;
+                    }
+                }
+                n--;
+            } while (swapped);
+            return list;
+        }
+        public int[] SortDsc(int[] list)
+        {
+            int n = list.Length;
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < n; i++)
+                {
+                    if (list[i] > list[i - 1])
+                    {
+                        int temp = list[i];
+                        list[i] = list[i - 1];
+                        list[i - 1] = temp;
+                        swapped = true;
+                    }
+                }
+                n--;
+            } while (swapped);
+            return list;
+        }
+    }
+}
--- ADS/Program.cs
+++ ADS/Program.cs
@@ -1,4 +1,5 @@
-﻿using ADS.Problems;
+﻿using ADS.Algorithm.SortingAlgorithm;
+using ADS.Problems;
 using BenchmarkDotNet.Running;
 using System.Diagnostics;
 
@@ -7,16 +8,22 @@ internal class Program
     private static void Main(string[] args)
     {
         Random random = new Random();
-
-
-        MoveZeroes obj = new MoveZeroes();
+        BubbleSort bubbleSort = new BubbleSort();
         int[] data = new int[] {-9,1,0,39,2,1};
         //for (int i = 0; i < 9; i++)
         //{
         //    data[i] = random.Next(20);
         //}
-        int[] result = obj.MoveZeroesMethod(data);
+        int[] result = bubbleSort.SortAsc(data);
         foreach (int i in result)
+        {
+            Console.WriteLine("Result:{0}", i);
+        }
+
+        Console.WriteLine("-------------------------------------------");
+
+        int[] result1 = bubbleSort.SortDsc(data);
+        foreach (int i in result1)
         {
             Console.WriteLine("Result:{0}", i);
         }
