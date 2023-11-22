83b0d5d0bb8c719b781e4d4e570be4ddb0d30501
A.K.M Maruful Islam <148320455+maruful106@users.noreply.github.com>
2023-11-22 12:44:38 +0600

    Dependency Injection

---
+++ DependencyInjection/CustomerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class CustomerService
+    {
+        private readonly ILogger _logger;
+
+        public CustomerService(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void AddCustomer(string customerName)
+        {
+            _logger.Log(customerName);
+        }
+    }
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,36 @@
+﻿using DependencyInjection;
+using System.Reflection;
+
+internal class Program
+{
+    /// <summary>
+    /// Dependency Injection(DI) is a software design pattern that deals with how components(objects or services) obtain their dependencies.
+    /// In simple terms, it is a technique in which the dependencies of a component are provided to it rather than the component creating 
+    /// or managing those dependencies itself.The primary goal of dependency injection is to achieve loose coupling between components, 
+    /// making the system more modular, maintainable, and testable.
+    /// 
+    /// Injection Types:
+
+    /// Constructor Injection:
+    ///     Dependencies are injected through the constructor of the dependent class.
+    /// Setter Injection:
+    ///     Dependencies are injected through setter methods of the dependent class.
+    /// Method Injection:
+    ///     Dependencies are injected through methods of the dependent class.
+    ///     
+    /// Inversion of Control (IoC):
+    ///     Dependency Injection is often associated with Inversion of Control.In IoC, the control over the flow of the program is inverted.
+    ///     Instead of a component controlling the creation and management of its dependencies, the responsibility is moved to an external 
+    ///     entity (the DI Container).
+    /// Loose Coupling:
+    ///     Dependency Injection promotes loose coupling between components.Components don't need to be aware of how their dependencies are 
+    ///     created or managed; they only rely on interfaces or abstract types.
+    /// </summary>
+    /// <param name="args"></param>
+    private static void Main(string[] args)
+    {
+        ILogger logger = new Logger();
+        CustomerService customerService = new CustomerService(logger);
+        customerService.AddCustomer("this is a customer.");
+    }
+}
