13c8230de524fc289f0403a86cf19ab1639cca2c
A.K.M Maruful Islam <148320455+maruful106@users.noreply.github.com>
2024-01-23 12:29:23 +0600

    Roman Number

---
+++ ADS/Problems/RomanNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADS.Problems
+{
+    public class RomanNumber
+    {
+        public int RomanToIntValue(string roman)
+        {
+            Dictionary<string, int> map = new Dictionary<string, int>()
+            {
+                {"I",1 }, {"V",5}, {"X",10}, {"L",50}, {"C",100},{"D",500},{"M",1000}
+            };
+
+            int totalCount = 0;
+            foreach (char word in roman)
+            {
+                totalCount = totalCount + map[word.ToString()];
+            }
+            Console.WriteLine(totalCount);
+            return totalCount;
+        }
+    }
+}
--- ADS/Program.cs
+++ ADS/Program.cs
@@ -1,31 +1,14 @@
 ﻿using ADS.Algorithm.SortingAlgorithm;
 using ADS.Problems;
 using BenchmarkDotNet.Running;
+using Microsoft.Diagnostics.Symbols;
 using System.Diagnostics;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Random random = new Random();
-        BubbleSort bubbleSort = new BubbleSort();
-        int[] data = new int[] {-9,1,0,39,2,1};
-        //for (int i = 0; i < 9; i++)
-        //{
-        //    data[i] = random.Next(20);
-        //}
-        int[] result = bubbleSort.SortAsc(data);
-        foreach (int i in result)
-        {
-            Console.WriteLine("Result:{0}", i);
-        }
-
-        Console.WriteLine("-------------------------------------------");
-
-        int[] result1 = bubbleSort.SortDsc(data);
-        foreach (int i in result1)
-        {
-            Console.WriteLine("Result:{0}", i);
-        }
+        RomanNumber romanNumber = new RomanNumber();
+        int result = romanNumber.RomanToIntValue("XXVIIMCD");
     }
 }
