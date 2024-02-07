06552e13914e2e2ea38dae58de1d3a28a029c890
A.K.M Maruful Islam <148320455+maruful106@users.noreply.github.com>
2024-02-07 13:57:40 +0600

    FindContentChildren

---
+++ ADS/Problems/AssignCookies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADS.Problems
+{
+    public class AssignCookies
+    {
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+            int i = 0, j = 0, contentChildren = 0;
+            while (i < g.Length && j < s.Length)
+            {
+                Console.WriteLine("valie of i: " + i);
+                Console.WriteLine("valie of j: " + j);
+                if (s[j] >= g[i])
+                {
+                    contentChildren++;
+                    i++;
+                }
+                j++;
+            }
+            return contentChildren;
+        }
+    }
+}
--- ADS/Program.cs
+++ ADS/Program.cs
@@ -8,7 +8,15 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        RomanNumber romanNumber = new RomanNumber();
-        int result = romanNumber.RomanToIntValue("XXVIIMCD");
+        AssignCookies assignCookies = new AssignCookies();
+        // Example 1
+        //int[] g1 = { 1, 2, 3 };// children
+        //int[] s1 = { 1, 1 };// cookies
+        //Console.WriteLine(assignCookies.FindContentChildren(g1, s1)); // Output: 1
+
+        // Example 2
+        int[] g2 = { 1, 2 };
+        int[] s2 = { 1, 2, 3 };
+        Console.WriteLine(assignCookies.FindContentChildren(g2, s2)); // Output: 2
     }
 }
